6b6ce18386c3612e702d25f16f784cd75a368f02
Gabriela22204 <gabimiho.mk@gmail.com>
2025-02-16 11:39:59 -0300

    edited button text

--- src/components/AnimatedBox.tsx
+++ src/components/AnimatedBox.tsx
@@ -7,20 +7,22 @@ export default function AnimatedBox() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      
       <motion.div
         className="w-20 h-20 bg-blue-500 rounded-lg"
         animate={{
           y: isAnimating ? [0, -100, 0] : 0, // Animação de pulo
           rotate: isAnimating ? [0, 360] : 0, // Rotação
         }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
-      />
+        transition={{ duration: 0.7, ease: "easeInOut" }} 
+      /> ₍^. .^₎⟆
       <button
         onClick={() => setIsAnimating(!isAnimating)}
         className="mt-5 px-4 py-2 bg-green-500 rounded-lg hover:bg-green-600 transition"
       >
-        Pular 🚀
+        Jump!
       </button>
+      
     </div>
   );
 }
